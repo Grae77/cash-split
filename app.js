eee447f199330622d3f787febe1aa547ba643833
Abhiraj Chaudhuri <abhirajchaudhuri@gmail.com>
2023-03-24 20:32:49 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+document.getElementById("cash-split").addEventListener("submit", function(event) {
+    event.preventDefault();
+    var bill = parseFloat(document.getElementById("bill").value);
+    var people = parseFloat(document.getElementById("people").value);
+    var split_per_person = bill / people;
+    var split_round = split_per_person.toFixed(2);
+    document.getElementById("cash-split").reset();
+    document.getElementById("result").innerHTML = "Each person should pay: ₹" + split_round;
+});
